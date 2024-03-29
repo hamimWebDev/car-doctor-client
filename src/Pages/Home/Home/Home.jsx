e37ffa832502c81744c49c11f5ab84f42b07f2b4
hamimDev <mdhamimhowladerasif@gmail.com>
2024-03-29 16:28:40 +0600

    cteate Navbar and Footer

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            This is Home
+        </div>
+    );
+};
+
+export default Home;
