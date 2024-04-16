ef686c920bafb203f88b225ab4fcdafb464fb9e2
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-16 21:45:43 +0600

    .env

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -5,13 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeveUkAMRtOIclBFy0-wxYKdwV_rZ9qOI",
-  authDomain: "cars-doctor-6db53.firebaseapp.com",
-  projectId: "cars-doctor-6db53",
-  storageBucket: "cars-doctor-6db53.appspot.com",
-  messagingSenderId: "604086364448",
-  appId: "1:604086364448:web:9c802b3d36d505a0404f45",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
